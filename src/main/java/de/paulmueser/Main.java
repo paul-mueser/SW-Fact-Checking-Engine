b20fa5fe2e6e5fc8ca7f3fc899be69fde8d6f698
Paul Müser <kontakt@paulmueser.de>
2024-11-17 16:38:22 +0100

    add basic argument parsing

--- src/main/java/de/paulmueser/Main.java
+++ src/main/java/de/paulmueser/Main.java
@@ -1,7 +1,49 @@
 package de.paulmueser;
 
+import org.apache.commons.cli.*;
+
 public class Main {
+    private static String inputFilePath;
+    private static String outputFilePath = "./result/";
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        parseArguments(args);
+    }
+
+    private static void parseArguments(String[] args) {
+        Options options = new Options();
+        
+        // Add options here
+        options.addOption("h", "help", false, "Show available options");
+        options.addOption("i", "input", true, "Input file path");
+        options.addOption("o", "output", true, "Output file path");
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        
+        try {
+            // TODO: replace utility-name with the name to execute the program
+            CommandLine cmd = parser.parse(options, args);
+            
+            if (cmd.hasOption("h")) {
+                formatter.printHelp("utility-name", options);
+                System.exit(0);
+            }
+            
+            if (cmd.hasOption("i")) {
+                inputFilePath = cmd.getOptionValue("i");
+            } else {
+                throw new ParseException("The i (input) option is required");
+            }
+            
+            if (cmd.hasOption("o")) {
+                outputFilePath = cmd.getOptionValue("o");
+            }
+            
+        } catch (ParseException e) {
+            Log.e("ArgumentParser", e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
     }
 }
